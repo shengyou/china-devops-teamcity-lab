84c36311d811d18fcc885c7f42f425300bd20b78
Shengyou Fan <shengyoufan@gmail.com>
2021-09-18 00:56:52 +0800

    add a failed test

--- src/test/kotlin/io/kraftsman/ShoppingCartTest.kt
+++ src/test/kotlin/io/kraftsman/ShoppingCartTest.kt
@@ -19,6 +19,20 @@ class ShoppingCartTest : FunSpec({
             // Assert
             shoppingCart.totalPrice() shouldBe 200
         }
+
+        test("當加了 2 個商品後會回到商品數量為 2") {
+            // Arrange
+            val product1 = Product(id = 1, name = "Product 1", price = 100)
+            val product2 = Product(id = 1, name = "Product 1", price = 100)
+            val shoppingCart = ShoppingCart()
+
+            // Act
+            shoppingCart.add(product1)
+            shoppingCart.add(product2)
+
+            // Assert
+            shoppingCart.count() shouldBe 200
+        }
     }
 
 })
