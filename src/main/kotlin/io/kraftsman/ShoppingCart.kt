56252264241912ebbd41c372c641b788964dd661
Shengyou Fan <shengyoufan@gmail.com>
2021-09-19 22:30:53 +0800

    break the code style

--- src/main/kotlin/io/kraftsman/ShoppingCart.kt
+++ src/main/kotlin/io/kraftsman/ShoppingCart.kt
@@ -3,10 +3,13 @@ package io.kraftsman
 class ShoppingCart {
     private val products = mutableListOf<Product>()
 
-    fun add  (product:Product) {
-          products.add(product)}
+    fun add(product: Product) {
+        products.add(product)
+    }
 
-    fun totalPrice():Int{return products.sumOf{it.price}}
+    fun totalPrice(): Int {
+        return products.sumOf { it.price }
+    }
 
     fun count(): Int {
         return products.count()
