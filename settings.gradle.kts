d3242d9efec9b36d7629a2e6d5d5309e31912e7e
Shengyou Fan <shengyoufan@gmail.com>
2021-09-26 17:25:38 +0800

    add gradle enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
-
 rootProject.name = "shopping-cart"
 
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
